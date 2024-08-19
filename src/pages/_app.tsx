41b6207b207afb9ae894944e24f0056c76e78898
Mustafa-khann <mustafakhann050@gmail.com>
2024-08-19 14:04:42 +0500

    Deploy Ready!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <div>
+    <Head>
+      <title>IntelliHome</title>
+      <link rel="icon" type="image/svg" href="/logo.svg" />
+      {/* other meta tags and links */}
+    </Head>
+    <Component {...pageProps} />
+  </div>
 );
 
 export default MyApp;
